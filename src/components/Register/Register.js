52f8e944ba47968b34227c0349976bac7f4fe401
meloegel <meloegel@gmail.com>
2021-05-16 22:59:27 -0400

    code clean up for deployment

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
